32ad7e09c773251b92c9827bfaa0f98ce52cc6b4
Jorge-coder02 <jorgitenis@hotmail.es>
2025-03-14 16:56:30 +0100

    botones show password (reducers)

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useReducer, useState } from "react";
+import { useReducer, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import NavBarLinks from "../components/NavBarLinks";
@@ -21,9 +21,30 @@ function reducer(state, action) {
   };
 }
 
+const initialStatePassIsShown = {
+  password1: false,
+  password2: false,
+};
+
+function reducer2(state, action) {
+  switch (action.type) {
+    case "TOGGLE_PASSWORD1":
+      return { ...state, password1: !state.password1 };
+    case "TOGGLE_PASSWORD2":
+      return { ...state, password2: !state.password2 };
+    default:
+      return state;
+  }
+}
+
 function Register({}) {
   const [state, dispatch] = useReducer(reducer, initialState);
   const [msgRespuesta, setMsgRespuesta] = useState("");
+
+  const [stateBtnsPassword, dispatch2] = useReducer(
+    reducer2,
+    initialStatePassIsShown
+  );
 
   const [errores, setErrores] = useState({
     name: [],
@@ -66,19 +87,6 @@ function Register({}) {
     dispatch({ field: "password", value: password_generada });
     dispatch({ field: "password2", value: password_generada });
     dispatch({ field: "age", value: 20 });
-  };
-
-  // * Funciones validación: *
-  const comprobarVacio = (campo, valor) => {
-    if (valor.trim() === "") {
-      setErrores((prev_errores) => ({
-        ...prev_errores,
-        [campo]: [
-          ...(prev_errores[campo] || []),
-          `Field ${campo} can´t be empty`,
-        ],
-      }));
-    }
   };
 
   // 📋 Validar campos en cliente
@@ -216,31 +224,147 @@ function Register({}) {
                 </p>
               ))}
           </div>
-          <div>
+          <div className="[&>div>button]:w-8 [&>div>button]:p-1 [&>div>button>svg]:w-4">
             <label htmlFor="password">Password</label>
-            <input
-              onChange={handleChange}
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={state.password}
-            />
+            <div className="flex gap-x-3">
+              <input
+                className="mb-1 rounded-md w-full p-1.5"
+                onChange={handleChange}
+                type={stateBtnsPassword.password1 ? "text" : "password"}
+                id="password"
+                autoComplete="current-password"
+                value={state.password}
+              />
+              <button
+                type="button"
+                onClick={() => {
+                  dispatch2({ type: "TOGGLE_PASSWORD1" });
+                }}
+                className="btn btn-sm btn-primary"
+              >
+                {!stateBtnsPassword.password1 ? (
+                  <svg
+                    fill="none"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M7 6.36185C8.36209 5.55936 10.0212 5 12 5C18.3074 5 21.3671 10.6832 21.9109 11.808C21.9705 11.9311 21.9702 12.0694 21.9107 12.1926C21.5585 12.9208 20.1542 15.5545 17.5 17.3244M14 18.8001C13.3735 18.9286 12.7071 19 12 19C5.69265 19 2.63286 13.3168 2.08909 12.192C2.02953 12.0689 2.03049 11.9291 2.09008 11.8059C2.30875 11.3539 2.9298 10.1741 4 8.92114"
+                      stroke="black"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                    />
+                    <path
+                      d="M10 9.76389C10.5308 9.28885 11.2316 9 12 9C13.6569 9 15 10.3431 15 12C15 12.7684 14.7111 13.4692 14.2361 14"
+                      stroke="black"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                    />
+                    <path
+                      d="M3 3L21 21"
+                      stroke="black"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="feather feather-eye"
+                    fill="none"
+                    height="24"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                    <circle cx="12" cy="12" r="3" />
+                  </svg>
+                )}
+              </button>
+            </div>
             {errores.password.length > 0 &&
               errores.password.map((error, index) => (
                 <p key={index} className="text-red-500">
                   {error}
                 </p>
               ))}
-          </div>{" "}
-          <div>
-            <label htmlFor="email">Confirm password</label>
-            <input
-              onChange={handleChange}
-              type="password"
-              id="password2"
-              autoComplete="password2"
-              value={state.password2}
-            />
+          </div>
+          <div className="[&>div>button]:w-8 [&>div>button]:p-1 [&>div>button>svg]:w-4">
+            <label htmlFor="password">Password</label>
+            <div className="flex gap-x-3">
+              <input
+                className="mb-1 rounded-md w-full p-1.5"
+                onChange={handleChange}
+                type={stateBtnsPassword.password2 ? "text" : "password"}
+                id="password2"
+                autoComplete="current-password2"
+                value={state.password2}
+              />
+              <button
+                type="button"
+                onClick={() => {
+                  dispatch2({ type: "TOGGLE_PASSWORD2" });
+                }}
+                className="btn btn-sm btn-primary"
+              >
+                {!stateBtnsPassword.password2 ? (
+                  <svg
+                    fill="none"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M7 6.36185C8.36209 5.55936 10.0212 5 12 5C18.3074 5 21.3671 10.6832 21.9109 11.808C21.9705 11.9311 21.9702 12.0694 21.9107 12.1926C21.5585 12.9208 20.1542 15.5545 17.5 17.3244M14 18.8001C13.3735 18.9286 12.7071 19 12 19C5.69265 19 2.63286 13.3168 2.08909 12.192C2.02953 12.0689 2.03049 11.9291 2.09008 11.8059C2.30875 11.3539 2.9298 10.1741 4 8.92114"
+                      stroke="black"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                    />
+                    <path
+                      d="M10 9.76389C10.5308 9.28885 11.2316 9 12 9C13.6569 9 15 10.3431 15 12C15 12.7684 14.7111 13.4692 14.2361 14"
+                      stroke="black"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                    />
+                    <path
+                      d="M3 3L21 21"
+                      stroke="black"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="feather feather-eye"
+                    fill="none"
+                    height="24"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                    <circle cx="12" cy="12" r="3" />
+                  </svg>
+                )}
+              </button>
+            </div>
             {errores.password2.length > 0 &&
               errores.password2.map((error, index) => (
                 <p key={index} className="text-red-500">
